8533f1a69138e0c9c5c11e6ce713094d1a319c1d
Vaishnavi Bharti <vaishuridhu@gmail.com>
2025-02-09 13:50:18 +0530

    exporing get ,post , delete http methods

--- src/app.js
+++ src/app.js
@@ -2,16 +2,28 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello",(req, res) => {
-    res.send("Hello ");
+app.get("/user",(req,res) => {
+    res.send({ firstName: "Vaishnavi", lastName: "Bharti", age: 23});
 });
-app.use("/",(req, res) => {
-    res.send("Hello from server");
+app.post("/user",(req, res) => {
+//    console.log(" data saved sucessfully to db");
+   res.send("data saved succesfully to db");
+});
+app.delete("/user",(req, res) => {
+//    console.log(" data saved sucessfully to db");
+   res.send("data deleted");
 });
 
-app.use("/dashboard",(req, res) => {
-    res.send("Hello from dashboard");
-});
+// app.use("/hello",(req, res) => {
+//     res.send("Hello ");
+// });
+// app.use("/",(req, res) => {
+//     res.send("Hello from server");
+// });
+
+// app.use("/dashboard",(req, res) => {
+//     res.send("Hello from dashboard");
+// });
 app.listen(3000 , () =>{
     console.log("server is sucessfully running");
 });
